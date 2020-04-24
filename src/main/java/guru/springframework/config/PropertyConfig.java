aedb5d83884dddad8eafaaa2c31fcecdc6cd9d51
Martin van Zyl <martin.vanzyl45@gmail.com>
2020-04-24 18:25:14 +0200

    Populating Spring Beans from Properties file

---
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -0,0 +1,41 @@
+package guru.springframework.config;
+
+import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${guru.username}")
+    String user;
+
+    @Value("${guru.password}")
+    String password;
+
+    @Value("${guru.dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+
+        return fakeDataSource;
+    }
+
+    //John Thompson says PropertySourcesPlaceholderConfigurer will scan for and
+    //read the datasource.properties file in order to populate the above properties
+    //annotated by @Value
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
